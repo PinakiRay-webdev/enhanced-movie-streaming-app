4dae6909b5e7fa28ced8a29da740f77ede4d709b
PinakiRay-webdev <pinaki.mongodb@gmail.com>
2024-11-27 19:09:14 +0530

    enhancements in home page

--- src/pages/Home/Components/Banner.jsx
+++ src/pages/Home/Components/Banner.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect , useState , useRef } from 'react'
 import axios from 'axios';
 import { FaChevronCircleLeft , FaChevronCircleRight } from "react-icons/fa";
+import { CiCalendarDate } from "react-icons/ci";
 
+
+import imdb from '../../../assets/imdb.svg'
 const Banner = () => {
 
   const baseurl = import.meta.env.VITE_BASE_URL;
@@ -63,7 +66,33 @@ const Banner = () => {
           <div className='h-screen min-w-full' key={id} >
             <img className='w-full h-full object-cover' src={`https://image.tmdb.org/t/p/original${Element.backdrop_path}`} alt="" />
             <div className='banner-data absolute w-full h-screen top-0 flex items-center pl-16' >
-              <img className='w-[20rem]' src={`https://image.tmdb.org/t/p/original${logos[Element.id]}`} alt="" />
+              <div>
+              <img className='w-[25rem]' src={`https://image.tmdb.org/t/p/original${logos[Element.id]}`} alt="" />
+              <p className='text-gray-300 w-[35%] my-8' >{Element.overview}</p>
+              <footer className='flex items-end gap-12' >
+                <div>
+                  <img className='w-8' src={imdb} alt="" />
+                  <p className='text-amber-400 text-3xl font-semibold'>{`${Element.vote_average}`.slice(0 , 3)}<span className='text-white text-sm' > / 10</span> </p>
+                </div>
+
+                <div className='flex items-end gap-3'>
+                  <p className='text-amber-400 text-4xl' >
+                  <CiCalendarDate />
+                  </p>
+                  <p className='text-white font-semibold text-xl' >{`${Element.first_air_date || Element.release_date}`.slice(0,4)}</p>
+                </div>
+                <div className='flex items-end gap-3'>
+                  <p className='text-amber-400 font-semibold' >
+                  Available in : 
+                  </p>
+                  <p className='text-white font-semibold text-xl' >{`${Element.original_language}`.slice(0,4)}</p>
+                </div>
+              </footer>
+              <div className='flex gap-6 mt-8' >
+                <button className='bg-white py-1 px-4 rounded-full' >Watch now</button>
+                <button className='watchList-btn text-white py-1 px-4 rounded-full' >Add to watch list</button>
+              </div>
+              </div>
             </div>
           </div>
         ))}
