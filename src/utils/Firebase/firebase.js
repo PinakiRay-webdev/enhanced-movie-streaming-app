4d3027481640094209259354d5599ee52a791f93
PinakiRay-webdev <pinaki.mongodb@gmail.com>
2024-11-28 10:54:51 +0530

    implemted firebase for hosting and authentication

---
+++ src/utils/Firebase/firebase.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyCC5vD6FjyKQ-1zIEXpMGOKqNecBta0UCw",
+  authDomain: "cinewave-b7b8b.firebaseapp.com",
+  projectId: "cinewave-b7b8b",
+  storageBucket: "cinewave-b7b8b.firebasestorage.app",
+  messagingSenderId: "855914790146",
+  appId: "1:855914790146:web:66d357742ca19cb193161b",
+  measurementId: "G-1SJC7BW574"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
